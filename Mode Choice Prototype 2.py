088c95054c8471c4bf467de4c3ef11f18ce75727
calicofig <167523669+calicofig@users.noreply.github.com>
2024-07-14 11:23:30 +0100

    Renew Mode Choice Prototype 2.py

--- Mode Choice Prototype 2.py
+++ Mode Choice Prototype 2.py
@@ -25,3 +25,5 @@ edit_mode = Button(root, text='Edit Mode', command=confirm_message_edit, style='
 edit_mode.grid(row = 1, column = 2, sticky = E, padx = 50, pady = 50)
 
 root.mainloop()
+
+#adding a line to save the repository
